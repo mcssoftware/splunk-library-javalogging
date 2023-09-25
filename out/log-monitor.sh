e03836507f4fa5cda710effe8c2829a349ab2df8
Sameer Bhandari <sameer@mcssoftwaresolutions.com>
2023-09-25 08:23:51 -0400

    Update log-monitor.sh

--- out/log-monitor.sh
+++ out/log-monitor.sh
@@ -16,7 +16,6 @@ SPARK_HOME=$DB_HOME/spark
 SPARK_CONF_DIR=$SPARK_HOME/conf
 
 echo "copying splunk library lavalogger"
-cp -f /dbfs/databricks/splunk/splunk-library-javalogging-1.11.8.jar /databricks/jars
 for jarfile in "okhttp-4.9.3.jar" "gson-2.9.0.jar" "splunk-library-javalogging-1.11.7.jar" "okio-3.5.0.jar" "okio-jvm-3.5.0.jar"
 do
     cp -f "/dbfs/databricks/splunk/$jarfile" /databricks/jars
@@ -30,13 +29,17 @@ do
 LOG4J_CONFIG_FILE="$SPARK_HOME/dbconf/log4j/$log4jDirectory/log4j2.xml"
 echo "BEGIN: Updating $LOG4J_CONFIG_FILE with Log Analytics appender"
 
-CONTENT="\ \ <SplunkHttp  name=\"$LOGGER_NAME\" url=\"https://prd-p-nexmd.splunkcloud.com\"\n\ \ \ \ token=\"51cac61f-4363-4ce5-9274-0293c4160d94\" host=\"clusterid\" index=\"databricks\" source=\"databricks\"\n\ \ \ \ sourcetype=\"dbx\" messageFormat=\"text\" \n\ \ \ \ disableCertificateValidation=\"true\">\n\ \ \ \ <PatternLayout pattern=\"%m\"/>\n\ \ </SplunkHttp>"
+CONTENT="\ \ <SplunkHttp  name=\"$LOGGER_NAME\" url=\"https://prd-p-nexmd.splunkcloud.com:8088\"\n\ \ \ \ token=\"51cac61f-4363-4ce5-9274-0293c4160d94\" host=\"clusterid\" index=\"databricks\" source=\"databricks\"\n\ \ \ \ sourcetype=\"dbx\" messageFormat=\"text\" \n\ \ \ \ disableCertificateValidation=\"true\">\n\ \ \ \ <PatternLayout pattern=\"%m\"/>\n\ \ </SplunkHttp>"
 C=$(echo $CONTENT | sed 's/\//\\\//g')
 sed -i "/<\/Appenders>/ s/.*/${C}\n&/" $LOG4J_CONFIG_FILE
 
 CONTENT="\ \ \ \ \ \ \  <AppenderRef ref=\"$LOGGER_NAME\"/>"
 C=$(echo $CONTENT | sed 's/\//\\\//g')
 sed -i "/<\/Root>/ s/.*/${C}\n&/" $LOG4J_CONFIG_FILE
+
+CONTENT="\ \ \ <Logger name=\"$LOGGER_NAME\" level=\"ALL\"> \ \ \ \ \<AppenderRef ref=\"$LOGGER_NAME\"/> \ \ \ </Logger>"
+C=$(echo $CONTENT | sed 's/\//\\\//g')
+sed -i "/<\/Loggers>/ s/.*/${C}\n&/" $LOG4J_CONFIG_FILE
 
 sed -i 's/packages="\([^"]*\)"/packages="\1,com.splunk.logging"/' $LOG4J_CONFIG_FILE
 
